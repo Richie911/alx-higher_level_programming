f2570365a652c7653e34e97217028440ed39bf95
Richie911 <56179749+Richie911@users.noreply.github.com>
2023-12-20 00:11:13 +0000

    all

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A square module"""
+
+
+class Square:
+    """A square Class"""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of side of the square.
+        """
+        self.__size = size
