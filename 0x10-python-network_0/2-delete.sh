21b49af0c50a40c1385c87ee3eea1d662b1a6347
Richie911 <oohrichmond.k@gmail.com>
2024-03-28 15:28:27 +0000

    fix

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
 #sends a DELETE request to the URL
+
 curl -s -X DELETE "${1}"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
 #displays all HTTP methods the server will accept
+
 curl -s -I "${1}" | grep "^Allow: .*" | cut -d " " -f 2-
